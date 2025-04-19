d15c5fa2a3a78f96933e0fea58ff9b2794d8bde7
twst-pmfr <Nastya.stash@mail.ru>
2025-04-19 17:20:05 +0300

    Update Lab4_1.py

--- Lab4_1.py
+++ Lab4_1.py
@@ -6,16 +6,16 @@ count = 1
 current_date = datetime.now()
 for i in range(num):
     if user == num:
-        print('Вы правильно угадали число', num, '.', 'Попытки: ', count, '.', 'Время', current_date)
+        print('Вы правильно угадали число', num, '.', 'Попытки: ', count, '.', 'Время:', current_date)
         break
     if user > num:
-        print('Число больше заданного', 'Время', current_date)
+        print('Число больше заданного', 'Время:', current_date)
         count += 1
         user = int(input('Напишите число: '))
     if user < num:
-        print('Число меньше заданного', 'Время', current_date)
+        print('Число меньше заданного', 'Время:', current_date)
         count += 1
         user = int(input('Напишите число: '))
     if count == 10:
-        print('Попытки закончились! Заданное число', num, '.', 'Попытки: ', count, '.', 'Время', current_date)
+        print('Попытки закончились! Заданное число', num, '.', 'Попытки: ', count, '.', 'Время:', current_date)
         break
